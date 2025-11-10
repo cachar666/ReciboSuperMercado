742316934f1ca4360bff9c1d071bb68593756b84
Carlos Chaves <cachar@gmail.com>
2025-11-10 16:31:42 -0500

    🔴 NoPermitirProductosDuplicados_PorDescripcion

--- Test/Test_CatalogoProductos.cs
+++ Test/Test_CatalogoProductos.cs
@@ -19,4 +19,22 @@ public class Test_CatalogoProductos
         // Assert
         catalogo.CantidadProductos().Should().Be(1);
     }
+    [Fact]
+    public void NoPermitirProductosDuplicados_PorDescripcion()
+    {
+        // Arrange
+        var catalogo = new Catalogo();
+        var p1 = new Producto("Manzana");
+        var p2 = new Producto("Manzana"); // mismo nombre, diferente instancia
+
+        catalogo.AgregarProducto(p1, 3.5m);
+
+        // Act
+        Action act = () => catalogo.AgregarProducto(p2, 3.8m);
+
+        // Assert
+        act.Should().Throw<InvalidOperationException>()
+            .WithMessage("El producto 'Manzana' ya existe en el catálogo.");
+        catalogo.CantidadProductos().Should().Be(1);
+    }
 }
