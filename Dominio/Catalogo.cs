b40b85aad9987eb98ff25a02bee7828b3cbf8485
Carlos Chaves <cachar@gmail.com>
2025-11-10 16:26:46 -0500

    🟢 AgregarProducto_DeberiaIncrementarCantidadDeProductos

---
+++ Dominio/Catalogo.cs
@@ -0,0 +1,16 @@
+﻿namespace ReciboSuperMercado.Dominio;
+
+public class Catalogo
+{
+    private readonly Dictionary<Producto, decimal> _precios = new();
+
+    public void AgregarProducto(Producto producto, decimal precio)
+    {
+        _precios[producto] = precio;
+    }
+
+    public int CantidadProductos()
+    {
+        return _precios.Count;
+    }
+}
---
+++ Dominio/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace ReciboSuperMercado.Dominio;
+
+public class Producto
+{
+    public string Nombre { get; }
+
+    public Producto(string nombre)
+    {
+        Nombre = nombre;
+    }
+}
