5d2b4cb894c758756557d2b8716c65b8328ea1c7
Carlos Chaves <cachar@gmail.com>
2025-11-10 16:39:41 -0500

    🔴 ObtenerPrecio_DeberiaLanzarExcepcion_SiProductoNoExiste

--- Test/Test_CatalogoProductos.cs
+++ Test/Test_CatalogoProductos.cs
@@ -2,54 +2,62 @@
 using FluentAssertions;
 using Xunit;
 
-namespace ReciboSuperMercado;
+namespace ReciboSuperMercado.Test;
 
 public class Test_CatalogoProductos
 {
     [Fact]
     public void AgregarProducto_DeberiaIncrementarCantidadDeProductos()
     {
-        // Arrange
         var catalogo = new Catalogo();
         var producto = new Producto("Manzana");
 
-        // Act
         catalogo.AgregarProducto(producto, 3.5m);
 
-        // Assert
         catalogo.CantidadProductos().Should().Be(1);
     }
+
     [Fact]
     public void NoPermitirProductosDuplicados_PorDescripcion()
     {
-        // Arrange
         var catalogo = new Catalogo();
         var p1 = new Producto("Manzana");
-        var p2 = new Producto("Manzana"); // mismo nombre, diferente instancia
+        var p2 = new Producto("Manzana");
 
         catalogo.AgregarProducto(p1, 3.5m);
 
-        // Act
         Action act = () => catalogo.AgregarProducto(p2, 3.8m);
 
-        // Assert
         act.Should().Throw<InvalidOperationException>()
             .WithMessage("El producto 'Manzana' ya existe en el catálogo.");
         catalogo.CantidadProductos().Should().Be(1);
     }
+
     [Fact]
     public void ObtenerPrecio_DeberiaRetornarPrecioRegistrado()
     {
-        // Arrange
         var catalogo = new Catalogo();
         var producto = new Producto("Banano");
 
         catalogo.AgregarProducto(producto, 4.2m);
 
-        // Act
         var precio = catalogo.ObtenerPrecio(producto);
 
-        // Assert
         precio.Should().Be(4.2m);
+    }
+
+    [Fact]
+    public void ObtenerPrecio_DeberiaLanzarExcepcion_SiProductoNoExiste()
+    {
+        // Arrange
+        var catalogo = new Catalogo();
+        var producto = new Producto("Pera");
+
+        // Act
+        Action act = () => catalogo.ObtenerPrecio(producto);
+
+        // Assert
+        act.Should().Throw<KeyNotFoundException>()
+            .WithMessage("El producto 'Pera' no existe en el catálogo.");
     }
 }
--- obj/Debug/net9.0/ReciboSuperMercado.AssemblyInfo.cs
+++ obj/Debug/net9.0/ReciboSuperMercado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReciboSuperMercado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a7f292c58a2a2fa99d0cf5d19bad1821475dd2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42968d787d7bb60218f6f9e3c86f7742898c03f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReciboSuperMercado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReciboSuperMercado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
