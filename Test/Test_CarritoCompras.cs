8d213bd80dcc602d803098799ece6d40f6c0385b
Carlos Chaves <cachar@gmail.com>
2025-11-10 16:55:51 -0500

    🔴 AgregarMultiplesProductos_DeberiaSumarTotalCorrectamente

--- Test/Test_CarritoCompras.cs
+++ Test/Test_CarritoCompras.cs
@@ -35,4 +35,26 @@ public class Test_CarritoCompras
         // Assert
         total.Should().Be(7.0m);
     }
+    
+    [Fact]
+    public void AgregarMultiplesProductos_DeberiaSumarTotalCorrectamente()
+    {
+        // Arrange
+        var catalogo = new Catalogo();
+        var manzana = new Producto("Manzana");
+        var pera = new Producto("Pera");
+
+        catalogo.AgregarProducto(manzana, 3.5m);
+        catalogo.AgregarProducto(pera, 4.0m);
+
+        var carrito = new CarritoDeCompras(catalogo);
+
+        // Act
+        carrito.AgregarProducto(manzana, 2); // 7.0
+        carrito.AgregarProducto(pera, 3);    // 12.0
+        var total = carrito.Total();         // 19.0
+
+        // Assert
+        total.Should().Be(19.0m);
+    }
 }
